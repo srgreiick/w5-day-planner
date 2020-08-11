acdd10196f69741b864d62de1831996ea88b0a20
sgreilick <samgreilick@gmail.com>
2020-08-11 15:33:24 -0600

    added an access to all of the hour blocks

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,17 @@
 //Prepare JS before running it
+
+
 $(document).ready(function () {
+
+let containerArray = $( ".container" ).get( );
+let eachHour = containerArray[0].children;
+console.log(eachHour.length);
+
+for (let i = 0; i < eachHour.length; i++) {
+    console.log(eachHour[i]);
+    
     
 
+}
 
 })
