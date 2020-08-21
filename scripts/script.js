af5b74e47c4a5df4e8c9ca9ce894ff2c8a323a13
sgreilick <samgreilick@gmail.com>
2020-08-20 20:46:25 -0600

    local storage is functional

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,43 @@
 //Prepare JS before running it
 
 
+
+
+
+
 $(document).ready(function () {
-
+let currentDate = moment().format(" Do MMMM YYYY");
+$("#currentDay").text("--------"+currentDate+" --------");
+let i = 0
+let now = moment().format("HH:MM")
 let containerArray = $( ".container" ).get( );
-let eachHour = containerArray[0].children;
-console.log(eachHour.length);
+let eachHourArray = containerArray[0].children;
+console.log(containerArray)
+console.log(eachHourArray)
 
-for (let i = 0; i < eachHour.length; i++) {
-    console.log(eachHour[i]);
-    
-    
 
+
+let eventClass;
+let event;
+let allBlocks = document.getElementById('masterBlock')
+
+console.log(allBlocks)
+allBlocks.addEventListener('click', (e) => {
+    eventClass = e.target.className
+    event = e.target
+    run()
+})
+run = function(){
+    console.log(eventClass)
+    if (eventClass === "far fa-save") {
+        console.log(event.parentElement.parentElement.children[1].value)
+        console.log(event.parentElement.parentElement.children[0].textContent)
+        localStorage.setItem(event.parentElement.parentElement.children[0].textContent,event.parentElement.parentElement.children[1].value)
+    }
+    else{
+    }
+    
 }
+
 
 })
