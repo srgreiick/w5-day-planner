3233435fcf51e4a0e80b79e397fd2704e166ff59
sgreilick <samgreilick@gmail.com>
2020-08-21 10:16:03 -0600

    now gets from local storage and populates the notes

--- scripts/script.js
+++ scripts/script.js
@@ -12,8 +12,6 @@ let i = 0
 let now = moment().format("HH:MM")
 let containerArray = $( ".container" ).get( );
 let eachHourArray = containerArray[0].children;
-console.log(containerArray)
-console.log(eachHourArray)
 
 
 
@@ -21,23 +19,29 @@ let eventClass;
 let event;
 let allBlocks = document.getElementById('masterBlock')
 
-console.log(allBlocks)
+
 allBlocks.addEventListener('click', (e) => {
     eventClass = e.target.className
     event = e.target
     run()
 })
+
 run = function(){
     console.log(eventClass)
     if (eventClass === "far fa-save") {
-        console.log(event.parentElement.parentElement.children[1].value)
-        console.log(event.parentElement.parentElement.children[0].textContent)
-        localStorage.setItem(event.parentElement.parentElement.children[0].textContent,event.parentElement.parentElement.children[1].value)
+        let keyPre = event.parentElement.parentElement.children[0].textContent.substring(0, 3)
+        let key = parseInt(keyPre);
+        localStorage.setItem(key,event.parentElement.parentElement.children[1].value)
     }
-    else{
-    }
-    
 }
-
-
+for (let i = 0; i < 8; i++) {
+   
+    let notes = localStorage.getItem(i+7)
+    console.log(notes)
+    allBlocks.children[i].children[1].textContent = notes
+    //allBlocks.children[1].textContent = notes
+}
+     
 })
+
+
